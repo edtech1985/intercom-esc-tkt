ec94ea0c0e608638cf7af8e29f245f8568ea2346
edtech1985 <edtech1985@gmail.com>
2025-06-02 15:01:21 -0300

    tirar texts

--- src/app/api/initialize/route.ts
+++ src/app/api/initialize/route.ts
@@ -15,23 +15,11 @@ export async function POST() {
             style: "header",
           },
           {
-            type: "text",
-            id: "tooltipTextAnalise",
-            text: "Está acontecendo um incidente Crítico na Plataforma Digibee e Cliente já reportou no suporte. Precisa de uma análise para o entendimento da causa.",
-            style: "paragraph",
-          },
-          {
             type: "button",
             label: "Precisa de uma análise imediata",
             style: "primary",
             id: "submit_button_pipeline",
             action: { type: "submit" },
-          },
-          {
-            type: "text",
-            id: "tooltipTextOcioso",
-            text: "Aberto Ticket de Incidente - Cliente ocioso no chat (Cliente parou de responder ou não está colaborando com testes ou análises essenciais).",
-            style: "paragraph",
           },
           {
             type: "button",
@@ -45,7 +33,7 @@ export async function POST() {
             id: "clientesTexto",
             text: "(Fleury, HDI, Assaí, Redeban, Salon Line, Localiza, Itaú, Vivo, Mariner, Grupo El Comercio)",
             align: "center",
-            style: "paragraph",
+            style: "label",
           },
         ],
       },
